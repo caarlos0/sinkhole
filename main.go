1933ca35fb9f90dc44870039e643c18c753a5292
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-07-30 14:44:02 -0300

    feat: handle sigterm
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -15,7 +18,9 @@ func main() {
 		addr = ":" + p
 	}
 	log.Println("listening on", addr)
-	http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if sleep := r.URL.Query().Get("sleep"); sleep != "" {
 			d, err := time.ParseDuration(sleep)
 			if err != nil {
@@ -32,5 +37,34 @@ func main() {
 		defer r.Body.Close()
 		log.Println(r.URL, "body:", string(bts))
 		fmt.Fprintln(w, "ok")
-	}))
+	})
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Println("server started")
+
+	<-done
+	log.Println("server stop requested")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer func() {
+		// extra handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown failed:%+v", err)
+	}
+	log.Print("server exit properly")
 }
